e25b4d53411be6680f0576b04c41c13473773d96
Acacio de Lima <limadeacacio@gmail.com>
2025-05-08 08:35:12 -0300

    feat: refactor metadata handling and remove unused getUrl utility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import "./globals.css";
 
-import { type Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { getUrl } from "@/utils/url";
-
+import { constructMetadata } from "@/lib/construct-metadata";
 import { ClientOnly } from "@/components/client-only";
 
 const geistSans = Geist({
@@ -17,38 +15,9 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: {
-    default: "GTA VI Countdown",
-    template: `%s - GTA VI Countdown`,
-  },
-  description:
-    "A simple and stylish countdown to the release of Grand Theft Auto VI. Let the wait begin!",
-  openGraph: {
-    title: "GTA VI Countdown",
-    description:
-      "A simple and stylish countdown to the release of Grand Theft Auto VI. Let the wait begin!",
-    images: [
-      {
-        url: "/opengraph-image.jpg",
-        alt: "GTA VI Countdown",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "GTA VI Countdown",
-    description:
-      "A simple and stylish countdown to the release of Grand Theft Auto VI. Let the wait begin!",
-    images: [{ url: "/twitter-image.jpg" }],
-  },
-  icons: "/favicon.ico",
-  metadataBase: new URL(getUrl("")),
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
+export const metadata = constructMetadata({
+  title: "GTA VI Countdown",
+});
 
 export default function RootLayout({
   children,
