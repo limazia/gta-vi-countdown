5f56b06290d4774b574979828f6d7aeaf18b3443
Acacio de Lima <limadeacacio@gmail.com>
2025-05-19 13:37:55 -0300

    refactor: update font handling to use Inter font and streamline layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import "./globals.css";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import { constructMetadata } from "@/lib/construct-metadata";
 import { ClientOnly } from "@/components/client-only";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -27,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         <ClientOnly>{children}</ClientOnly>
       </body>
     </html>
