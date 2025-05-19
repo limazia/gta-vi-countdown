422edd700b603b4c4eb684dc31ba067e1b410974
Acacio de Lima <limadeacacio@gmail.com>
2025-05-19 13:20:50 -0300

    refactor: enhance metadata construction by introducing TitleTemplate interface and improving URL handling

--- src/lib/construct-metadata.ts
+++ src/lib/construct-metadata.ts
@@ -1,5 +1,21 @@
 import { Metadata } from "next";
 
+interface TitleTemplate {
+  default: string;
+  template: string;
+  absolute?: string;
+}
+
+function getUrl(url: string) {
+  if (typeof window !== "undefined") return url;
+
+  if (process.env.NEXT_PUBLIC_URL) {
+    return process.env.NEXT_PUBLIC_URL;
+  } else {
+    return "http://localhost:3000";
+  }
+}
+
 export function constructMetadata({
   title = "GTA VI Countdown",
   description = "A simple and stylish countdown to the release of Grand Theft Auto VI. Let the wait begin!",
@@ -8,43 +24,49 @@ export function constructMetadata({
   noIndex = false,
   useOgApi = false,
 }: {
-  title?: string;
+  title?: string | TitleTemplate;
   description?: string;
   image?: string;
   icons?: string;
   noIndex?: boolean;
   useOgApi?: boolean;
 } = {}): Metadata {
-  const baseUrl = process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000";
+  const ogApiUrl = `/api/og?cacheBuster=${
+    new Date().toISOString().split("T")[0]
+  }`;
 
-  const finalImageUrl = `${baseUrl}${useOgApi ? "/api/og" : image}`;
+  const finalImageUrl = useOgApi ? ogApiUrl : image;
 
   return {
-    title,
+    title: {
+      default: typeof title === "string" ? title : title.default,
+      template:
+        typeof title === "string"
+          ? title
+          : title.template ?? `%s - ${title.default}`,
+      absolute: typeof title === "string" ? undefined : title.absolute,
+    },
     description,
-    metadataBase: new URL(baseUrl),
     openGraph: {
       title,
       description,
-      url: baseUrl,
-      siteName: "GTA VI Countdown",
       images: [
         {
           url: finalImageUrl,
+          alt: typeof title === "string" ? title : undefined,
           width: 1200,
           height: 630,
-          alt: title,
         },
       ],
-      type: "website",
     },
     twitter: {
       card: "summary_large_image",
       title,
       description,
-      images: [finalImageUrl],
+      images: [{ url: finalImageUrl }],
     },
     icons,
+    metadataBase: new URL(getUrl("")),
     ...(noIndex && {
       robots: {
         index: true,
